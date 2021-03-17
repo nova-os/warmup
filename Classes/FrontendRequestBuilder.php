1ee4e3540d8d8107c50d8b32ae7b55426ee5229c
Oliver Schneider <o.schneider@nova.digital>
2021-03-17 10:53:24 +0100

    Use External HTTP requests

--- Classes/FrontendRequestBuilder.php
+++ Classes/FrontendRequestBuilder.php
@@ -12,26 +12,11 @@ namespace B13\Warmup;
  */
 
 
-use B13\Warmup\Authentication\FrontendUserGroupInjector;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
-use TYPO3\CMS\Core\Cache\CacheManager;
-use TYPO3\CMS\Core\Core\ApplicationContext;
-use TYPO3\CMS\Core\Core\Environment;
-use TYPO3\CMS\Core\Error\Http\PageNotFoundException;
-use TYPO3\CMS\Core\Http\ImmediateResponseException;
-use TYPO3\CMS\Core\Http\MiddlewareDispatcher;
-use TYPO3\CMS\Core\Http\MiddlewareStackResolver;
-use TYPO3\CMS\Core\Http\ServerRequest;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Package\PackageManager;
-use TYPO3\CMS\Core\Service\DependencyOrderingService;
+use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\Exception\RequiredArgumentMissingException;
-use TYPO3\CMS\Extbase\Service\EnvironmentService;
-use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
-use TYPO3\CMS\Frontend\Http\RequestHandler;
+
 
 /**
  * Simulates a HTTP request for TYPO3 Frontend within the same request.
@@ -40,188 +25,10 @@ use TYPO3\CMS\Frontend\Http\RequestHandler;
  */
 class FrontendRequestBuilder
 {
-    private $originalUser;
-
-    private $backedUpEnvironment = [];
-
-    private $originalEnvironmentService;
-
-    private function prepare()
-    {
-        $this->originalUser = $GLOBALS['BE_USER'];
-        $this->backupEnvironment();
-        $this->initializeEnvironmentForNonCliCall(GeneralUtility::getApplicationContext());
-
-        $this->originalEnvironmentService = GeneralUtility::makeInstance(EnvironmentService::class);
-        if (!class_exists(Typo3Version::class) || (new Typo3Version)->getMajorVersion() < 10) {
-            $environmentService = new class extends EnvironmentService {
-                public function isEnvironmentInFrontendMode()
-                {
-                    return true;
-                }
-                public function isEnvironmentInBackendMode()
-                {
-                    return false;
-                }
-                public function isEnvironmentInCliMode()
-                {
-                    return false;
-                }
-            };
-            GeneralUtility::setSingletonInstance(Dispatcher::class, new Dispatcher());
-        } else {
-            // TYPO3 v10 ships with strict types
-            $environmentService = new class extends EnvironmentService {
-                public function isEnvironmentInFrontendMode(): bool
-                {
-                    return true;
-                }
-                public function isEnvironmentInBackendMode(): bool
-                {
-                    return false;
-                }
-                public function isEnvironmentInCliMode()
-                {
-                    return false;
-                }
-            };
-
-        }
-        GeneralUtility::setSingletonInstance(EnvironmentService::class, $environmentService);
-
-        $GLOBALS['BE_USER'] = null;
-        unset($GLOBALS['TSFE']);
-    }
-
-    private function restore()
-    {
-        $this->disableFrontendGroupAuthService();
-        $GLOBALS['BE_USER'] = $this->originalUser;
-        GeneralUtility::setSingletonInstance(EnvironmentService::class, $this->originalEnvironmentService);
-        unset($GLOBALS['TSFE']);
-        $this->restoreEnvironment();
-    }
-
-
     public function buildRequestForPage(UriInterface $uri, ?int $frontendUserId, $frontendUserGroups = []): ?ResponseInterface
     {
-        $this->prepare();
-        $request = new ServerRequest($uri, 'GET');
-
-        if (!empty($frontendUserGroups)) {
-            $this->activateFrontendGroupAuthService($frontendUserGroups, $frontendUserId);
-        }
-
-        $response = null;
-        try {
-            $response = $this->executeFrontendRequest($request);
-        } catch (ImmediateResponseException $e) {
-            $response = $e->getResponse();
-        } catch (RequiredArgumentMissingException $e) {
-            // @todo: log
-        } catch (PageNotFoundException $e) {
-            // @todo: log
-        } catch (\Throwable $e) {
-            // @todo: log
-            // var_dump(get_class($e));
-        }
-
-        $this->restore();
-
+        $requestFactory = GeneralUtility::makeInstance(RequestFactory::class);
+        $response= $requestFactory->request("" . $uri, 'GET', []);
         return $response;
-    }
-
-    /**
-     * @param ServerRequestInterface $request
-     * @return ResponseInterface
-     */
-    public function executeFrontendRequest(ServerRequestInterface $request): ResponseInterface
-    {
-        $dispatcher = $this->buildDispatcher();
-        return $dispatcher->handle($request);
-    }
-
-    /**
-     * @return MiddlewareDispatcher
-     * @throws \TYPO3\CMS\Core\Cache\Exception\InvalidDataException
-     * @throws \TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException
-     * @throws \TYPO3\CMS\Core\Exception
-     */
-    private function buildDispatcher()
-    {
-        $requestHandler = GeneralUtility::makeInstance(RequestHandler::class);
-        $resolver = new MiddlewareStackResolver(
-            GeneralUtility::makeInstance(PackageManager::class),
-            GeneralUtility::makeInstance(DependencyOrderingService::class),
-            GeneralUtility::makeInstance(CacheManager::class)->getCache('cache_core')
-        );
-        $middlewares = $resolver->resolve('frontend');
-        return new MiddlewareDispatcher($requestHandler, $middlewares);
-    }
-
-    private function initializeEnvironmentForNonCliCall(ApplicationContext $applicationContext): void
-    {
-        Environment::initialize(
-            $applicationContext,
-            false,
-            Environment::isComposerMode(),
-            Environment::getProjectPath(),
-            Environment::getPublicPath(),
-            Environment::getVarPath(),
-            Environment::getConfigPath(),
-            Environment::getCurrentScript(),
-            Environment::isWindows() ? 'WINDOWS' : 'UNIX'
-        );
-    }
-
-    /**
-     * Helper method used in setUp() if $this->backupEnvironment is true
-     * to back up current state of the Environment::class
-     */
-    private function backupEnvironment(): void
-    {
-        $this->backedUpEnvironment['context'] = Environment::getContext();
-        $this->backedUpEnvironment['isCli'] = Environment::isCli();
-        $this->backedUpEnvironment['composerMode'] = Environment::isComposerMode();
-        $this->backedUpEnvironment['projectPath'] = Environment::getProjectPath();
-        $this->backedUpEnvironment['publicPath'] = Environment::getPublicPath();
-        $this->backedUpEnvironment['varPath'] = Environment::getVarPath();
-        $this->backedUpEnvironment['configPath'] = Environment::getConfigPath();
-        $this->backedUpEnvironment['currentScript'] = Environment::getCurrentScript();
-        $this->backedUpEnvironment['isOsWindows'] = Environment::isWindows();
-    }
-
-    /**
-     * Helper method used in tearDown() if $this->backupEnvironment is true
-     * to reset state of Environment::class
-     */
-    private function restoreEnvironment(): void
-    {
-        Environment::initialize(
-            $this->backedUpEnvironment['context'],
-            $this->backedUpEnvironment['isCli'],
-            $this->backedUpEnvironment['composerMode'],
-            $this->backedUpEnvironment['projectPath'],
-            $this->backedUpEnvironment['publicPath'],
-            $this->backedUpEnvironment['varPath'],
-            $this->backedUpEnvironment['configPath'],
-            $this->backedUpEnvironment['currentScript'],
-            $this->backedUpEnvironment['isOsWindows'] ? 'WINDOWS' : 'UNIX'
-        );
-    }
-
-    private function activateFrontendGroupAuthService(array $userGroups, ?int $specificUserId)
-    {
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['available'] = true;
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['alwaysActiveGroups'] = $userGroups;
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['requestUser'] = $specificUserId ?? false;
-
-    }
-
-    private function disableFrontendGroupAuthService()
-    {
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['available'] = false;
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['alwaysActiveGroups'] = [];
-        $GLOBALS['T3_SERVICES']['auth'][FrontendUserGroupInjector::class]['requestUser'] = false;
     }
 }
